f126e67a0a018493c44d0561b32a2c8243249be7
tekna28 <salma.jeghloul@gmail.com>
2023-08-10 14:40:56 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints exactly and that piece of art is useful"
+ *- Dora Korpar, 2015-10-19 to the standard error.
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
