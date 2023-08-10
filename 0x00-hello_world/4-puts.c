05b73e38a83720ea7c822e22abd7a3033f2ae74f
tekna28 <salma.jeghloul@gmail.com>
2023-08-10 12:37:50 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch[] = "Programming is like building a multilingual puzzle";
+puts(ch);
+return (0);
+}
