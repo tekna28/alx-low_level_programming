5076a35dee9b71b4132152ffc053b7fc64e8e38d
tekna28 <salma.jeghloul@gmail.com>
2023-08-12 07:31:34 -0700

    print comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,23 +7,20 @@
  */
 int main(void)
 {
-int n1 = 0;
+int n1;
 int n2;
-while (n1 < 99)
+for (n1 = 0; n1 <= 9; n1++)
 {
-n2 = n1 + 1;
-while (n2 < 99)
+for (n2 = n1 + 1; n2 <= 9; n2++)
 {
-putchar(n1);
-putchar(n2);
-if (n1 != 8 && n2 != 9)
+putchar(n1 + '0');
+putchar(n2 + '0');
+if (n1 < 8 || n2 < 9)
 {
 putchar(',');
 putchar(' ');
 }
-n2++;
 }
-n1++;
 }
 putchar('\n');
 return (0);
