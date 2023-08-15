19f6ddda173abf94b80363b4b12927303390fefd
tekna28 <salma.jeghloul@gmail.com>
2023-08-15 15:29:19 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include  "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - computes the absolute value of an integer.
+ *@n: integer
+ *
+ * Return: absolute value of an integer (Success)
+ */
+int _abs(int n)
+{
+int i;
+i = abs(n);
+return (i);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,7 @@ void print_alphabet_x10(void);
 int _islower(char c);
 int _isalpha(char c);
 int print_sign(int n);
+int _abs(int);
 
 #endif
 
