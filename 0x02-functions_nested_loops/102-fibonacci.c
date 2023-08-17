7ce55c64a6bc84fcf5ccf758b7a884d78bc46b35
tekna28 <salma.jeghloul@gmail.com>
2023-08-16 17:40:48 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,32 +7,27 @@
  */
 int main(void)
 {
-long int count, one, two, next;
+long int i, one, two, next;
 
-count = 3;
 one = 1;
 two = 2;
-
-printf("%lu, ", one);
-printf("%lu, ", two);
 next = one + two;
 
-while (count <= 50)
+printf("%lu, %lu, ", one, two);
+
+for (i = 3; i <= 50; i++)
 {
-if (count == 50)
-{
-printf("%lu, \n", next);
-}
-else
+if (i < 50)
 {
 printf("%lu, ", next);
 }
-
+else if (i == 50)
+{
+printf("%lu\n", next);
+}
 one = two;
 two = next;
 next = one + two;
-
-count++;
 }
 return (0);
 }
