0ab2cc50d0152fc009606288552e773067cbd6e9
tekna28 <salma.jeghloul@gmail.com>
2023-08-24 14:19:58 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,21 +9,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i, j;
+int i;
 
-for (i = 0; s1[i] != '\0'; i++)
+i = 0;
+
+while (s1[i] != '\0' && s2[i] != '\0')
 {
-for (j = 0; s2[j] != '\0'; j++)
+if (s1[i] != s2[i])
 {
-if (s1[i] != s2[j])
-{
-return (s1[i] - s2[j]);
+return (s1[i] - s2[i]);
 }
-else
-{
-return (0);
-}
-}
+i++;
 }
 return (0);
 }
