29f17d2eb75094b973dcf82105d80ad1fe31ccf7
tekna28 <salma.jeghloul@gmail.com>
2023-09-11 09:21:20 -0700

    A new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef _HEADER_FILE_
+#define _HEADER_FILE_
+
+/**
+ * struct dog - Dog
+ *
+ *@name: name of the dog.
+ *@age: age of the dog.
+ *@owner: owner of the dog.
+ * Description: Dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+/**
+ *my_dog - typedef for dog structure.
+ */
+typedef struct dog my_dog;
+
+#endif
