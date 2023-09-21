aa890e840c3c5233316f0064e97ad00495c2c59c
tekna28 <salma.jeghloul@gmail.com>
2023-09-21 12:31:04 -0700

    A function that prints something before the main function is executed.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print - prints a sentence.
+ *
+ * Return: no return (Success)
+ */
+void print(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
