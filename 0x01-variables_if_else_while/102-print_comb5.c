922eaec98370404a941999bbc3464a22822089cb
tekna28 <salma.jeghloul@gmail.com>
2023-10-20 12:55:33 -0700

    deleted

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 int n1, n2;
-for (n1 = 0; n1 <= 99; n1++)
+for (n1 = 0; n1 < 99; n1++)
 {
-for (n2 = n1 + 1; n2 <= 99; n2++)
+for (n2 = n1 + 1; n2 < 100; n2++)
 {
 putchar((n1 / 10) + '0');
 putchar((n1 % 10) + '0');
